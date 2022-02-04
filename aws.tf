00af624625c1fd56918b817b29e7157aa98b4f30
rhbelson <rhbelson@gmail.com>
2022-02-04 19:24:50 +0000

    added lambda zip as path

--- aws.tf
+++ aws.tf
@@ -78,7 +78,8 @@ resource "aws_lambda_function" "edsDataPlaneLambda" {
   function_name    = "edsDataPlaneLambda"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.lambda_handler"
-  source_code_hash = filebase64sha256("edsDataPlaneLambda.zip")
+  # source_code_hash = filebase64sha256("edsDataPlaneLambda.zip")
+  source_code_hash = filebase64sha256("${path.module}/edsDataPlaneLambda.zip")
   runtime          = "python3.8"
   timeout          = 10
   environment {
