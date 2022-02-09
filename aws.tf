daadc52ede5b1d7aaff38993f7a61c7bd54cdcfb
rhbelson <rhbelson@gmail.com>
2022-02-09 20:45:37 +0000

    modifying filepath with relative path

--- aws.tf
+++ aws.tf
@@ -74,10 +74,11 @@ resource "aws_iam_role_policy_attachment" "iam_for_lambda_ssmAccess" {
 }
 
 resource "aws_lambda_function" "edsDataPlaneLambda" {
+  filename         = "${path.module}/edsDataPlaneLambda.zip"
   function_name    = "edsDataPlaneLambda"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.lambda_handler"
-  source_code_hash = filebase64sha256("${path.module}/edsDataPlaneLambda.zip")
+  # source_code_hash = filebase64sha256("${path.module}/edsDataPlaneLambda.zip")
   runtime          = "python3.8"
   timeout          = 10
   environment {
